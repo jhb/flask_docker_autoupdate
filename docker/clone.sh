4b765833264c54d566d5f74464a142a0918d0243
Joerg Baach <mail@baach.de>
2022-03-30 14:47:11 +0200

    proper paths

--- docker/clone.sh
+++ docker/clone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/jhb/flask_docker_autoupdate.git
-/home/gopoc/flask_docker_autoupdate/venv/bin/pip install -r flask_docker_autoupdate.git/server/requirements.txt
+/home/gopoc/venv/bin/pip install -r flask_docker_autoupdate.git/server/requirements.txt
