95f5fded03b57c49764c4d6a7845fb962e9ab385
Joerg Baach <mail@baach.de>
2022-04-08 12:13:06 +0200

    this works

--- docker/update.sh
+++ docker/update.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-git -C /home/gopoc/repo pull
-/home/gopoc/venv/bin/pip install -r /home/gopoc/repo/server/requirements.txt
+echo 'updating'
+while :
+do
+  date
+  git -C /home/gopoc/repo pull
+  /home/gopoc/venv/bin/pip install -r /home/gopoc/repo/server/requirements.txt
+  sleep 60
+done
+
+# ./venv/bin/pip list | grep -i yaml
+# ./venv/bin/pip uninstall -y pyyaml
